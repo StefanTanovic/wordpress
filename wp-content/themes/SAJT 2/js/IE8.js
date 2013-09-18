a6838a7046bee3ec1ee6121380b87d4831dc8c55
Stefan <stefantanovic@gmail.com>
2013-09-18 12:52:17 +0200

    2nd commit
    proba

--- wp-content/themes/SAJT 2/js/IE8.js
+++
@@ -1,9 +0,0 @@
-// JavaScript Document
-document.createElement('header');
- document.createElement('hgroup');
- document.createElement('nav');
- document.createElement('menu');
- document.createElement('section');
- document.createElement('article');
- document.createElement('aside');
- document.createElement('footer');
