f25713f04c888a2e05e991e7dc6f7e35a8e6c98a
Stefan <stefantanovic@gmail.com>
2013-09-19 11:22:50 +0200

    extension changed and header bug fixed

---
+++ wp-content/themes/NewTheme/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" <?php language_attributes(); ?> >
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo('charset'); ?>" />
+<meta name="generator" content="WordPress <?php bloginfo('version'); ?>" />
+	<!-- leave this for stats -->
+<meta name="description" content="<?php bloginfo('description'); ?>" />
+
+<link rel="stylesheet" href="<?php bloginfo('stylesheet_url'); ?>" type="text/css" />
+<link rel="alternate" type="aplication/rss+xml" title="RSS Feed" href="<?php bloginfo('rss2_url'); ?>" />
+<link rel="alternate" type="aplication/atom+xml" title="Atom Feed" href="<?php bloginfo('atom_url'); ?>" />
+<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
+
+<title><?php bloginfo('name'); ?><?php wp_title(); ?></title>
+
+<?php wp_head(); ?>
+</head>
+
+<body <?php body_class(); ?>>
+<div id="header">
+	<h1>My site</h1>
+</div> <!--#header -->
+
+<div id="main">
+	<h2>Foo.</h2>
+    <p>Lorem ipsum dolor sit amet, ad ipsum verear accumsan mea, mucius ceteros dissentias ex sed. Option expetendis quo ex. At pro propriae voluptaria comprehensam. Quo nihil partem accommodare in, diam sanctus vix ut, etiam error audire in nam.
+Vitae tantas salutatus ea pro, mea purto honestatis interesset id. Probo aliquam feugait at eam. Rebum viris graece mea ea. Has dicam veniam nonumy ei, no graecis menandri comprehensam vis.</p>
+</div> <!-- #main -->
+
+<div id="sidebar">
+	<ul>
+    	<li><a href="#">Page One</a></li>
+        <li><a href="#">Page Two</a></li>
+        <li><a href="#">Page Three</a></li>
+    </ul>
+</div> <!-- #sidebar -->
+
+<div id="footer">
+	<p>&copy; 2010 My Site.</p>
+</div> <!-- #footer -->
+       
+</body>
+</html>
